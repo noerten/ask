5c1864ea77fd75fbd07e29060c12ff6be056d46a
noerten <darkstria@gmail.com>
2016-03-13 20:09:21 +0300

    lesson3 added models.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'qa'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -76,10 +77,14 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myproject',
+        'USER': 'enth',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
-}
+} 
 
 
 # Password validation
