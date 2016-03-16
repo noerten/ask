4d4d4de22216c5334e2797fdccd635e73bb9b891
noerten <darkstria@gmail.com>
2016-03-16 20:24:48 +0300

    web init

--- web_init.sh
+++ web_init.sh
@@ -1,6 +1,14 @@
 sudo ln -sf /home/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
+
+sudo apt-get update
+sudo apt-get install libmysqlclient-dev
+sudo apt-get install python3-dev python-dev libxml2-dev libxslt1-dev zlib1g-dev
+sudo sed -i 's/skip-external-locking/skip-external-locking\ninnodb_use_native_aio = 0/' /etc/mysql/my.cnf
+sudo /etc/init.d/mysql restart
+mysql -uroot -e "create database stepic;"
+mysql -uroot -e "CREATE USER 'trasea'@'localhost' IDENTIFIED BY 'trasea';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON * . * TO 'trasea'@'localhost';"
+mysql -uroot -e "FLUSH PRIVILEGES;"
 sudo /etc/init.d/nginx restart
-
-
 
